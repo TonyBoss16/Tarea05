c81193adb4d461c540bc48b3dd74d97e332f59eb
AnthonyC <a.contrerasf16@gmail.com>
2019-10-02 22:54:57 -0400

    hice el lisView pero ni idea de como pasar al ultimo activity

--- app/src/main/java/com/example/tarea05/SegundoActivity.java
+++ app/src/main/java/com/example/tarea05/SegundoActivity.java
@@ -3,10 +3,18 @@ package com.example.tarea05;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class SegundoActivity extends Activity {
 
+    private TextView tv1;
+    private ListView lv1;
+
+    private String nombres [] = {"el mas bello", "el mejor", "el malo", "el grande", "el que mejor programa" };
+    private String edades [] = {   "10",           "15",       "20",       "25",            "30"};
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,19 @@ public class SegundoActivity extends Activity {
         TextView texto_resultado = (TextView) findViewById(R.id.tv_nombre);
 
         texto_resultado.setText(""+nombre);
+
+        lv1 = (ListView) findViewById(R.id.lv_cualidades);
+
+        ArrayAdapter <String> adapter= new ArrayAdapter<String>(this,R.layout.list_item_atributos, nombres);
+        lv1.setAdapter(adapter);
+
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                
+            }
+        });
+
     }
 
     public void mostrar_datos (View view) {
