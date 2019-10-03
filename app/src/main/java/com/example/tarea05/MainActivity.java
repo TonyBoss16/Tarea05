88f2c45fecf7134d27dec0e5fe7581105ab4dd1e
AnthonyC <a.contrerasf16@gmail.com>
2019-10-02 22:20:13 -0400

    Ya aprendi a pasar datos

--- app/src/main/java/com/example/tarea05/MainActivity.java
+++ app/src/main/java/com/example/tarea05/MainActivity.java
@@ -5,9 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +16,12 @@ public class MainActivity extends AppCompatActivity {
     }
     public void ejecutar_segundo_activity (View view) {
 
+        EditText cuadro_nombre = (EditText) findViewById(R.id.edt_name);
+
         Intent i = new Intent(this, SegundoActivity.class);
+
+        i.putExtra("nombre",""+cuadro_nombre.getText() );
+        //i.putExtra("nombre2","Soy Anthony");
 
         startActivity(i);
     }
--- app/src/main/java/com/example/tarea05/SegundoActivity.java
+++ app/src/main/java/com/example/tarea05/SegundoActivity.java
@@ -2,6 +2,8 @@ package com.example.tarea05;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SegundoActivity extends Activity {
 
@@ -9,5 +11,18 @@ public class SegundoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.segundo_activity);
+
+        Bundle datos = getIntent().getExtras();
+
+        String nombre = datos.getString("nombre");
+
+        TextView texto_resultado = (TextView) findViewById(R.id.tv_nombre);
+
+        texto_resultado.setText(""+nombre);
+    }
+
+    public void mostrar_datos (View view) {
+
+
     }
 }
